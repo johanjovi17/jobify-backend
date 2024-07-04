8365d984eee1cf8dfb2012f8a2611ce233d214bf
Johan Fernandes <94102288+johanjovi17@users.noreply.github.com>
2024-07-04 13:03:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(
   cors({
     origin: 
       "https://jobify-frontend-inky.vercel.app",
-    ,
+    
     // Update with your Vercel domain
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
